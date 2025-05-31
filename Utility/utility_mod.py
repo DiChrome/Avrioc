b0a188c8a55f72ac02921017b72a129682a80117
DiChrome <geoathul@gmail.com>
2025-05-31 21:57:18 +0400

    edit

--- Utility/utility_mod.py
+++ Utility/utility_mod.py
@@ -18,6 +18,7 @@ class Util:
         params["arch_bit"] = struct.calcsize("P") * 8
         params["device"] = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         params["platform"] = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"{params["platform"]=}")
         return params
 
     def increment_model_index(yaml_path):
